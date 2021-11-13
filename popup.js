77cddcd6fac055c6a758b165f409076d4567848f
jessicaxiao11 <jessicaxiao11@gmail.com>
2021-11-12 19:30:34 -0500

    create recipe box on webpage

--- popup.js
+++ popup.js
@@ -16,6 +16,25 @@ function showRecipe() {
   console.log(ingredientsText);
   console.log(instructionsText);
 
+  // create Recipe Buddy interface
+
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://fonts.googleapis.com/css2?family=Pattaya&display=swap';
+  document.head.appendChild(link);
+
+  const box = document.createElement("div");
+  box.id = "recipeBox";
+
+  document.body.insertBefore(box, document.body.firstChild);
+
+  const title = document.createElement("p");
+  const text = document.createTextNode("Recipe Buddy");
+  title.appendChild(text);
+  title.style.fontFamily = "Pattaya";
+  box.appendChild(title);
+
+
   // add ingredients to HTML
 
   // add first instruction to HTML

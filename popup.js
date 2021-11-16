c57ac865620b70b0a80178c97e28e4ac19e3931d
jessicaxiao11 <jessicaxiao11@gmail.com>
2021-11-16 09:02:28 -0500

    adding more of static UI elements

--- popup.js
+++ popup.js
@@ -13,9 +13,6 @@ function showRecipe() {
     instructionsText.push(instructions[i].textContent);
   }
 
-  console.log(ingredientsText);
-  console.log(instructionsText);
-
   // create Recipe Buddy interface
 
   var link = document.createElement('link');
@@ -23,21 +20,37 @@ function showRecipe() {
   link.href = 'https://fonts.googleapis.com/css2?family=Pattaya&display=swap';
   document.head.appendChild(link);
 
+  // create recipe buddy overlay
   const box = document.createElement("div");
   box.id = "recipeBox";
-
   document.body.insertBefore(box, document.body.firstChild);
 
+  // add recipe buddy title
   const title = document.createElement("p");
   const text = document.createTextNode("Recipe Buddy");
+  title.id = "title";
   title.appendChild(text);
-  title.style.fontFamily = "Pattaya";
   box.appendChild(title);
 
-
   // add ingredients to HTML
+  const ingredientsBox = document.createElement("div");
+  ingredientsBox.id = "ingredientsBox";
+  box.appendChild(ingredientsBox);
 
   // add first instruction to HTML
+  const instructionsBox = document.createElement("div");
+  instructionsBox.id = "instructionsBox";
+  box.appendChild(instructionsBox);
+
+  // add Add to Planner button
+  const addToPlanner = document.createElement("div");
+  const addToPlannerP = document.createElement("p");
+  const addToPlannerText = document.createTextNode("Add to Planner");
+  addToPlanner.id = "addToPlanner";
+  addToPlannerP.appendChild(addToPlannerText);
+  addToPlanner.appendChild(addToPlannerP);
+  // addToPlanner.addEventListener('click', )
+  box.appendChild(addToPlanner);
 }
 
 showRecipe();
